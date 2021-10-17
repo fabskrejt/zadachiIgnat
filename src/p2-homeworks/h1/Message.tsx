665dcb04b3a2ff481f300e4efe311b16fa47658c
Vladimir Shits <rav9aa@gmail.com>
2021-10-17 18:01:27 +0300

    first commit

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import mStyle from './Message.module.css'
+
+type PropsTypeOfMessage = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: PropsTypeOfMessage) {
+    return (
+        <div className={mStyle.messageBox}>
+            <div className={mStyle.avatar}>
+                <img src={props.avatar}/>
+            </div>
+            <div className={mStyle.textBlock}>
+                <div>
+                    <div className={mStyle.name}>{props.name}</div>
+                    <div className={mStyle.message}>{props.message}</div>
+                </div>
+                <div className={mStyle.sendTime}>{props.time}</div>
+            </div>
+        </div>
+    )
+}
+
+export default Message
