16384f2ea40ddb863d17f4e8d493a16a022022fc
Vladimir Shits <rav9aa@gmail.com>
2022-01-26 23:57:22 +0300

    added themeReducer

--- src/p2-homeworks/h10/bll/store.ts
+++ src/p2-homeworks/h10/bll/store.ts
@@ -1,8 +1,10 @@
 import {loadingReducer} from './loadingReducer'
 import {combineReducers, createStore} from "redux";
+import {themeReducer} from "../../h12/bll/themeReducer";
 
 const reducers = combineReducers({
     loading: loadingReducer,
+    theme: themeReducer,
 
 })
 
@@ -13,4 +15,4 @@ export default store
 export type AppStoreType = ReturnType<typeof reducers>
 
 // @ts-ignore
- window.store = store // for dev
+window.store = store // for dev
